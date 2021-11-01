c12f1df4321bec350ecaa653681c2d07da78595c
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-10-31 21:22:55 -0500

    ADD: se finalizo proyecto

--- app/src/main/java/com/maximoprog/appkotlin/MainActivity.kt
+++ app/src/main/java/com/maximoprog/appkotlin/MainActivity.kt
@@ -17,7 +17,10 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root;
         setContentView(view);
 
-        binding.zeroBtn.setOnClickListener { this.numberPress("1") }
+        binding.resultText.text = "0"
+        this.operation = NO_OPERATION;
+
+        binding.zeroBtn.setOnClickListener { this.numberPress("0") }
         binding.oneBtn.setOnClickListener { this.numberPress("1") }
         binding.twoBtn.setOnClickListener { this.numberPress("2") }
         binding.threeBtn.setOnClickListener { this.numberPress("3") }
@@ -27,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         binding.sevenBtn.setOnClickListener { this.numberPress("7") }
         binding.eightBtn.setOnClickListener { this.numberPress("8") }
         binding.nineBtn.setOnClickListener { this.numberPress("9") }
+        binding.decimalBtn.setOnClickListener { this.numberPress(".") }
 
 //        OPERATIONS
         binding.addBtn.setOnClickListener { this.operationPress(ADD) }
@@ -34,11 +38,51 @@ class MainActivity : AppCompatActivity() {
         binding.multiplyBtn.setOnClickListener { this.operationPress(MULTIPLY) }
         binding.divideBtn.setOnClickListener { this.operationPress(DIVISION) }
 
+        binding.clearBtn.setOnClickListener { this.clearAction() }
+
+        binding.equalBtn.setOnClickListener { this.equalsAction() }
+    }
+
+    private fun equalsAction() {
+        var result = when (this.operation) {
+            ADD -> this.num1 + this.num2;
+            SUBTRACT -> this.num1 - this.num2;
+            MULTIPLY -> this.num1 * this.num2;
+            DIVISION -> this.num1 / this.num2;
+            else -> 0;
+        }
+        this.num1 = result as Double;
+
+        if ("$result".endsWith(".0")) {
+            binding.resultText.text = "$result".replace(".0", "");
+        } else {
+            binding.resultText.text = "%.2f".format(result);
+        };
+
+    }
+
+    private fun clearAction() {
+        this.num1 = 0.0;
+        this.num2 = 0.0;
+        binding.resultText.text = "0";
+//        this.operation = NO_OPERATION;
     }
 
     @SuppressLint("SetTextI18n")
     private fun numberPress(digit: String) {
-        binding.resultText.text = "${binding.resultText.text}${digit}";
+
+        if (binding.resultText.text == "0" && digit != ".") {
+            binding.resultText.text = "$digit";
+        } else {
+            binding.resultText.text = "${binding.resultText.text}${digit}";
+
+        }
+        if (this.operation == NO_OPERATION) {
+            this.num1 = binding.resultText.text.toString().toDouble();
+        } else {
+            this.num2 = binding.resultText.text.toString().toDouble();
+
+        }
     }
 
     private fun operationPress(operation: Int) {
@@ -49,9 +93,9 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         const val ADD = 1;
-        const val SUBTRACT = 1;
-        const val MULTIPLY = 1;
-        const val DIVISION = 1;
-        const val NO_OPERATION = 1;
+        const val SUBTRACT = 2;
+        const val MULTIPLY = 3;
+        const val DIVISION = 4;
+        const val NO_OPERATION = 5;
     }
 }
